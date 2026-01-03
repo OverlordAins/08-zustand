e05215c95a36f6d0b0b042d673f16465bd83046c
Daniil <stifflingdagger@gmail.com>
2026-01-03 21:39:28 +0200

    default fix

--- app/notes/filter/@sidebar/default.tsx
+++ app/notes/filter/@sidebar/default.tsx
@@ -1,3 +1,30 @@
+// import Link from 'next/link';
+// import css from './Sidebar.module.css';
+
+// const SidebarDefault = () => {
+//   const tags = ['Todo', 'Work', 'Personal', 'Meeting', 'Shopping'];
+
+//   return (
+//     <nav className={css.sidebarNav}>
+//       <Link href="/notes/filter/all" className={css.navItem}>
+//         All notes
+//       </Link>
+
+//       {tags.map(tag => (
+//         <Link
+//           key={tag}
+//           href={`/notes/filter/${tag.toLowerCase()}`}
+//           className={css.navItem}
+//         >
+//           {tag}
+//         </Link>
+//       ))}
+//     </nav>
+//   );
+// };
+
+// export default SidebarDefault;
+
 import Link from 'next/link';
 import css from './Sidebar.module.css';
 
@@ -11,11 +38,7 @@ const SidebarDefault = () => {
       </Link>
 
       {tags.map(tag => (
-        <Link
-          key={tag}
-          href={`/notes/filter/${tag.toLowerCase()}`}
-          className={css.navItem}
-        >
+        <Link key={tag} href={`/notes/filter/${tag}`} className={css.navItem}>
           {tag}
         </Link>
       ))}
